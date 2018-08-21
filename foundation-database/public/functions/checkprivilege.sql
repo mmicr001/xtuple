c9da5756ea853514db5093725d613b00c4f4dada
Dave Anderson <dave@pentuple.co.nz>
2018-08-22 10:24:31 +1200

    Update checkprivilege function to handle superuser check and check for ALL privileges assigned

--- foundation-database/public/functions/checkprivilege.sql
+++ foundation-database/public/functions/checkprivilege.sql
@@ -6,28 +6,49 @@ RETURNS BOOLEAN STABLE AS $$
 -- Copyright (c) 1999-2018 by OpenMFG LLC, d/b/a xTuple.
 -- See www.xtuple.com/CPAL for the full text of the software license.
 DECLARE
-  _result TEXT;
+  _priv   TEXT[];
+  _all    BOOLEAN := false;
+  _count  INTEGER;
+  _result INTEGER;
 BEGIN
-  SELECT priv_id INTO _result
+/*
+  if pPrivilege contains #superuser then check for isDBA() privilege
+  if pPrivileges are separated by a space then return TRUE if ANY privileges are assigned
+  if pPrivileges are separated by a plus then return TRUE only if ALL privileges are assigned
+*/
+
+  IF (pPrivilege = '#superuser') THEN
+    RETURN isDBA();
+  END IF;
+
+  _priv := string_to_array(REPLACE(pPrivilege,'+',' '), ' ');
+
+  IF (POSITION('+' IN pPrivilege) > 0) THEN
+    _all := true;
+    _count := array_length(_priv, 1);
+  END IF;
+
+  SELECT COUNT(*) INTO _result
     FROM priv, grppriv, usrgrp
    WHERE((usrgrp_grp_id=grppriv_grp_id)
      AND (grppriv_priv_id=priv_id)
-     AND (priv_name IN (SELECT unnest(string_to_array(pPrivilege, ' '))))
+     AND (priv_name IN (SELECT unnest(_priv)))
      AND (usrgrp_username=pUsername));
-  IF (FOUND) THEN
-    RETURN true;
+
+  IF ((_all AND _result =_count) OR (NOT(_all) AND _result > 0)) THEN
+    RETURN true;  
   END IF;
 
-  SELECT priv_id INTO _result
+  SELECT COUNT(*) INTO _result
   FROM priv, usrpriv
   WHERE ((priv_id=usrpriv_priv_id)
-  AND (priv_name IN (SELECT unnest(string_to_array(pPrivilege, ' '))))
+  AND (priv_name IN (SELECT unnest(_priv)))
   AND (usrpriv_username=pUsername));
 
-  IF (FOUND) THEN
-    RETURN true;
-  ELSE
-    RETURN false;
+  IF ((_all AND _result =_count) OR (NOT(_all) AND _result > 0)) THEN
+    RETURN true;  
   END IF;
+  
+  RETURN false;
 END;
 $$ LANGUAGE 'plpgsql';
