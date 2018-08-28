c0c1e64c4b15281283f1106fd748fedc165d4500
Dave Anderson <dave@pentuple.co.nz>
2018-08-28 12:58:27 +1200

    Rework function to handle all manner of AND/OR/ANDOR scenarios

--- foundation-database/public/functions/checkprivilege.sql
+++ foundation-database/public/functions/checkprivilege.sql
@@ -6,49 +6,74 @@ RETURNS BOOLEAN STABLE AS $$
 -- Copyright (c) 1999-2018 by OpenMFG LLC, d/b/a xTuple.
 -- See www.xtuple.com/CPAL for the full text of the software license.
 DECLARE
-  _priv   TEXT[];
-  _all    BOOLEAN := false;
-  _count  INTEGER;
-  _result INTEGER;
+  _priv     TEXT[];
+  _privAnd  TEXT[];
+  _andCount INTEGER;
+  _r        RECORD;
+  _result   INTEGER;
 BEGIN
 /*
   if pPrivilege contains #superuser then check for isDBA() privilege
   if pPrivileges are separated by a space then return TRUE if ANY privileges are assigned
   if pPrivileges are separated by a plus then return TRUE only if ALL privileges are assigned
 */
-
   IF (pPrivilege = '#superuser') THEN
     RETURN isDBA();
   END IF;
 
-  _priv := string_to_array(REPLACE(pPrivilege,'+',' '), ' ');
+  _priv := string_to_array(pPrivilege, ' ');
 
-  IF (POSITION('+' IN pPrivilege) > 0) THEN
-    _all := true;
-    _count := array_length(_priv, 1);
-  END IF;
+  FOR _r IN
+    SELECT unnest(_priv) AS priv
+  LOOP
+    IF (NOT _r.priv LIKE '%+%') THEN
+   -- Check OR
+      SELECT SUM(count) INTO _result
+      FROM (
+        SELECT COUNT(*)
+          FROM priv, grppriv, usrgrp
+         WHERE usrgrp_grp_id=grppriv_grp_id
+           AND grppriv_priv_id=priv_id
+           AND priv_name = _r.priv
+           AND usrgrp_username=pUsername
+        UNION ALL
+        SELECT COUNT(*)
+          FROM priv, usrpriv
+         WHERE priv_id=usrpriv_priv_id
+           AND priv_name = _r.priv
+           AND usrpriv_username=pUsername) foo;
 
-  SELECT COUNT(*) INTO _result
-    FROM priv, grppriv, usrgrp
-   WHERE((usrgrp_grp_id=grppriv_grp_id)
-     AND (grppriv_priv_id=priv_id)
-     AND (priv_name IN (SELECT unnest(_priv)))
-     AND (usrgrp_username=pUsername));
+      IF (_result > 0) THEN
+        RETURN true;
+      END IF;
 
-  IF ((_all AND _result =_count) OR (NOT(_all) AND _result > 0)) THEN
-    RETURN true;  
-  END IF;
+    ELSE
+  -- Check AND
+      _privAnd := string_to_array(_r.priv, '+');
+      _andCount := array_length(_privAnd, 1);
 
-  SELECT COUNT(*) INTO _result
-  FROM priv, usrpriv
-  WHERE ((priv_id=usrpriv_priv_id)
-  AND (priv_name IN (SELECT unnest(_priv)))
-  AND (usrpriv_username=pUsername));
+      SELECT SUM(count) INTO _result
+      FROM (
+        SELECT COUNT(*)
+          FROM priv, grppriv, usrgrp
+         WHERE usrgrp_grp_id=grppriv_grp_id
+           AND grppriv_priv_id=priv_id
+           AND priv_name IN (SELECT unnest(_privAnd))
+           AND usrgrp_username=pUsername
+        UNION ALL
+        SELECT COUNT(*)
+          FROM priv, usrpriv
+         WHERE priv_id=usrpriv_priv_id
+           AND priv_name IN (SELECT unnest(_privAnd))
+           AND usrpriv_username=pUsername) foo;
 
-  IF ((_all AND _result =_count) OR (NOT(_all) AND _result > 0)) THEN
-    RETURN true;  
-  END IF;
-  
+      IF (_result = _andCount) THEN
+        RETURN true;
+      END IF;
+
+    END IF;
+  END LOOP;
+
   RETURN false;
 END;
 $$ LANGUAGE 'plpgsql';
