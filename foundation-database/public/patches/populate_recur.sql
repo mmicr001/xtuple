61b1a3b2ca736a47e17642b9d9ea6a1e8906cdd0
Gilberto Rivera <31081918+interhawk@users.noreply.github.com>
2018-10-17 14:30:59 -0400

    adds on clonflict to recurrence type population script

--- foundation-database/public/patches/populate_recur.sql
+++ foundation-database/public/patches/populate_recur.sql
@@ -2,4 +2,5 @@ INSERT INTO recurtype (recurtype_type, recurtype_table, recurtype_donecheck,
                        recurtype_schedcol, recurtype_limit, recurtype_copyfunc, 
                        recurtype_copyargs, recurtype_delfunc) 
                VALUES ('SALESORDER', 'cohead', 'isRecurSODone(cohead_id)', 'cohead_orderdate',
-                       NULL, 'copycohead', '{INTEGER,TIMESTAMP WITH TIME ZONE}', NULL);
+                       NULL, 'copycohead', '{INTEGER,TIMESTAMP WITH TIME ZONE}', NULL) 
+          ON CONFLICT (recurtype_id) DO NOTHING;
