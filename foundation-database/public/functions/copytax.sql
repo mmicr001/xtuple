698fb583b17a3ca25751696ec0e18b80639049f2
Chris Cotnoir <ccotnoir@xtuple.com>
2018-10-17 14:56:47 -0400

    Address more comments

--- foundation-database/public/functions/copytax.sql
+++ foundation-database/public/functions/copytax.sql
@@ -33,105 +33,63 @@ BEGIN
       FROM taxhead
      WHERE taxhead_doc_type = pSourceType
        AND taxhead_doc_id = pSourceId;
-
-    FOR _r IN SELECT taxline_id
-                FROM taxhead
-                JOIN taxline ON taxhead_id = taxline_taxhead_id
-               WHERE taxhead_doc_type = pSourceType
-                 AND taxhead_doc_id = pSourceId
-                 AND taxline_line_type != 'L'
-    LOOP
-      INSERT INTO taxline (taxline_taxhead_id, taxline_line_type, taxline_line_id,
-                           taxline_linenumber, taxline_subnumber, taxline_number,
-                           taxline_item_number, taxline_shipfromaddr_line1,
-                           taxline_shipfromaddr_line2, taxline_shipfromaddr_line3,
-                           taxline_shipfromaddr_city, taxline_shipfromaddr_region,
-                           taxline_shipfromaddr_postalcode, taxline_shipfromaddr_country,
-                           taxline_taxtype_id, taxline_taxtype_external_code, taxline_qty,
-                           taxline_amount, taxline_extended)
-      SELECT taxhead_id, taxline_line_type, taxline_line_id,
-             taxline_linenumber, taxline_subnumber, taxline_number,
-             taxline_item_number, taxline_shipfromaddr_line1,
-             taxline_shipfromaddr_line2, taxline_shipfromaddr_line3,
-             taxline_shipfromaddr_city, taxline_shipfromaddr_region,
-             taxline_shipfromaddr_postalcode, taxline_shipfromaddr_country,
-             taxline_taxtype_id, taxline_taxtype_external_code, taxline_qty,
-             taxline_amount, taxline_extended
-        FROM taxhead, taxline
-       WHERE taxhead_doc_type = pTargetType
-         AND taxhead_doc_id = pTargetId
-         AND taxline_id = _r.taxline_id
-      RETURNING taxline_id INTO _taxlineid;
-
-      INSERT INTO taxdetail (taxdetail_taxline_id, taxdetail_taxable, taxdetail_tax_id,
-                             taxdetail_tax_code, taxdetail_taxclass_id, taxdetail_sequence,
-                             taxdetail_basis_tax_id, taxdetail_amount, taxdetail_percent,
-                             taxdetail_tax, taxdetail_tax_owed, taxdetail_paydate,
-                             taxdetail_tax_paid)
-      SELECT _taxlineid, taxdetail_taxable, taxdetail_tax_id,
-             taxdetail_tax_code, taxdetail_taxclass_id, taxdetail_sequence,
-             taxdetail_basis_tax_id, taxdetail_amount, taxdetail_percent,
-             taxdetail_tax, taxdetail_tax_owed, taxdetail_paydate,
-             taxdetail_tax_paid
-        FROM taxhead, taxdetail
-       WHERE taxhead_doc_type = pTargetType
-         AND taxhead_doc_id = pTargetId
-         AND taxdetail_taxline_id = _r.taxline_id;
-    END LOOP;
-  ELSE
-    FOR _r IN SELECT taxline_id
-                FROM taxline
-                JOIN taxhead ON taxline_taxhead_id = taxhead_id
-               WHERE taxhead_doc_type = pSourceType
-                 AND taxline_line_id = pSourceId
-    LOOP
-      INSERT INTO taxline (taxline_taxhead_id, taxline_line_type, taxline_line_id,
-                           taxline_linenumber, taxline_subnumber, taxline_number,
-                           taxline_item_number, taxline_shipfromaddr_line1,
-                           taxline_shipfromaddr_line2, taxline_shipfromaddr_line3,
-                           taxline_shipfromaddr_city, taxline_shipfromaddr_region,
-                           taxline_shipfromaddr_postalcode, taxline_shipfromaddr_country,
-                           taxline_taxtype_id, taxline_taxtype_external_code, taxline_qty,
-                           taxline_amount, taxline_extended)
-      SELECT taxhead_id, taxline_line_type, pTargetId,
-             taxline_linenumber, taxline_subnumber, taxline_number,
-             taxline_item_number, taxline_shipfromaddr_line1,
-             taxline_shipfromaddr_line2, taxline_shipfromaddr_line3,
-             taxline_shipfromaddr_city, taxline_shipfromaddr_region,
-             taxline_shipfromaddr_postalcode, taxline_shipfromaddr_country,
-             taxline_taxtype_id, taxline_taxtype_external_code, taxline_qty,
-             taxline_amount, taxline_extended
-        FROM taxhead, taxline
-       WHERE taxhead_doc_type = pTargetType
-         AND taxhead_doc_id = pTargetHeadId
-         AND taxline_id = _r.taxline_id
-      RETURNING taxline_id INTO _taxlineid;
-
-      INSERT INTO taxdetail (taxdetail_taxline_id, taxdetail_taxable, taxdetail_tax_id,
-                             taxdetail_tax_code, taxdetail_taxclass_id, taxdetail_sequence,
-                             taxdetail_basis_tax_id, taxdetail_amount, taxdetail_percent,
-                             taxdetail_tax, taxdetail_tax_owed, taxdetail_paydate,
-                             taxdetail_tax_paid)
-      SELECT _taxlineid, taxdetail_taxable, taxdetail_tax_id,
-             taxdetail_tax_code, taxdetail_taxclass_id, taxdetail_sequence,
-             taxdetail_basis_tax_id, taxdetail_amount, taxdetail_percent,
-             taxdetail_tax, taxdetail_tax_owed, taxdetail_paydate,
-             taxdetail_tax_paid
-        FROM taxhead, taxdetail
-       WHERE taxhead_doc_type = pTargetType
-         AND taxhead_doc_id = pTargetHeadId
-         AND taxdetail_taxline_id = _r.taxline_id;
-    END LOOP;
-
-    UPDATE taxhead
-       SET taxhead_valid = src.taxhead_valid
-      FROM taxline
-      JOIN taxhead src ON taxline_taxhead_id = src.taxhead_id
-     WHERE src.taxhead_doc_type = pSourceType
-       AND taxline_line_id = pSourceId
-       AND taxhead.taxhead_doc_type = pTargetType
-       AND taxhead.taxhead_doc_id = pTargetHeadId;
   END IF;
+
+  FOR _r IN SELECT taxline_id
+              FROM taxhead
+              JOIN taxline ON taxhead_id = taxline_taxhead_id
+             WHERE taxhead_doc_type = pSourceType
+               AND CASE WHEN pTargetHeadId IS NOT NULL
+                        THEN taxhead_doc_id = pSourceId AND taxline_line_type != 'L'
+                        ELSE taxline_line_id = pSourceId
+                    END
+  LOOP
+    INSERT INTO taxline (taxline_taxhead_id, taxline_line_type, taxline_line_id,
+                         taxline_linenumber, taxline_subnumber, taxline_number,
+                         taxline_item_number, taxline_shipfromaddr_line1,
+                         taxline_shipfromaddr_line2, taxline_shipfromaddr_line3,
+                         taxline_shipfromaddr_city, taxline_shipfromaddr_region,
+                         taxline_shipfromaddr_postalcode, taxline_shipfromaddr_country,
+                         taxline_taxtype_id, taxline_taxtype_external_code, taxline_qty,
+                         taxline_amount, taxline_extended)
+    SELECT taxhead_id, taxline_line_type, taxline_line_id,
+           taxline_linenumber, taxline_subnumber, taxline_number,
+           taxline_item_number, taxline_shipfromaddr_line1,
+           taxline_shipfromaddr_line2, taxline_shipfromaddr_line3,
+           taxline_shipfromaddr_city, taxline_shipfromaddr_region,
+           taxline_shipfromaddr_postalcode, taxline_shipfromaddr_country,
+           taxline_taxtype_id, taxline_taxtype_external_code, taxline_qty,
+           taxline_amount, taxline_extended
+      FROM taxhead, taxline
+     WHERE taxhead_doc_type = pTargetType
+       AND taxhead_doc_id = COALESCE(pTargetHeadId, pTargetId)
+       AND taxline_id = _r.taxline_id
+    RETURNING taxline_id INTO _taxlineid;
+
+    INSERT INTO taxdetail (taxdetail_taxline_id, taxdetail_taxable, taxdetail_tax_id,
+                           taxdetail_tax_code, taxdetail_taxclass_id, taxdetail_sequence,
+                           taxdetail_basis_tax_id, taxdetail_amount, taxdetail_percent,
+                           taxdetail_tax, taxdetail_tax_owed, taxdetail_paydate,
+                           taxdetail_tax_paid)
+    SELECT _taxlineid, taxdetail_taxable, taxdetail_tax_id,
+           taxdetail_tax_code, taxdetail_taxclass_id, taxdetail_sequence,
+           taxdetail_basis_tax_id, taxdetail_amount, taxdetail_percent,
+           taxdetail_tax, taxdetail_tax_owed, taxdetail_paydate,
+           taxdetail_tax_paid
+      FROM taxhead, taxdetail
+     WHERE taxhead_doc_type = pTargetType
+       AND taxhead_doc_id = COALESCE(pTargetHeadId, pTargetId)
+       AND taxdetail_taxline_id = _r.taxline_id;
+  END LOOP;
+
+  UPDATE taxhead
+     SET taxhead_valid = src.taxhead_valid
+    FROM taxline
+    JOIN taxhead src ON taxline_taxhead_id = src.taxhead_id
+   WHERE src.taxhead_doc_type = pSourceType
+     AND taxline_line_id = pSourceId
+     AND taxhead.taxhead_doc_type = pTargetType
+     AND taxhead.taxhead_doc_id = pTargetHeadId;
 
   RETURN TRUE;
 
--- foundation-database/public/tables/voitem.sql
+++ foundation-database/public/tables/voitem.sql
@@ -1,13 +1,13 @@
 SELECT xt.create_table('voitem', 'public');
 
-SELECT xt.add_column('voitem', 'voitem_id', 'SERIAL', 'PRIMARY KEY', 'public');
-SELECT xt.add_column('voitem', 'voitem_vohead_id', 'INTEGER', 'NULL', 'public');
-SELECT xt.add_column('voitem', 'voitem_poitem_id', 'INTEGER', 'NULL', 'public');
-SELECT xt.add_column('voitem', 'voitem_close', 'BOOLEAN', 'NULL', 'public');
-SELECT xt.add_column('voitem', 'voitem_qty', 'NUMERIC(18,6)', 'NULL', 'public');
-SELECT xt.add_column('voitem', 'voitem_freight', 'NUMERIC(16,4)', 'NOT NULL DEFAULT 0.0', 'public');
-SELECT xt.add_column('voitem', 'voitem_taxtype_id', 'INTEGER', 'NULL', 'public');
-SELECT xt.add_column('voitem', 'voitem_tax_exemption', 'TEXT', 'NULL', 'public');
+SELECT xt.add_column('voitem', 'voitem_id', 'SERIAL', 'PRIMARY KEY', 'public'),
+       xt.add_column('voitem', 'voitem_vohead_id', 'INTEGER', 'NULL', 'public'),
+       xt.add_column('voitem', 'voitem_poitem_id', 'INTEGER', 'NULL', 'public'),
+       xt.add_column('voitem', 'voitem_close', 'BOOLEAN', 'NULL', 'public'),
+       xt.add_column('voitem', 'voitem_qty', 'NUMERIC(18,6)', 'NULL', 'public'),
+       xt.add_column('voitem', 'voitem_freight', 'NUMERIC(16,4)', 'NOT NULL DEFAULT 0.0', 'public'),
+       xt.add_column('voitem', 'voitem_taxtype_id', 'INTEGER', 'NULL', 'public'),
+       xt.add_column('voitem', 'voitem_tax_exemption', 'TEXT', 'NULL', 'public');
 
 SELECT xt.add_constraint('voitem', 'voitem_voitem_taxtype_id_fkey', 'FOREIGN KEY (voitem_taxtype_id) REFERENCES taxtype(taxtype_id)', 'public');
 
