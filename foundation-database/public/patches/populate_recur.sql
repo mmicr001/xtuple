f3161cbee22623021602cf14da77dfeab0502bb6
Gilberto Rivera <31081918+interhawk@users.noreply.github.com>
2018-10-23 13:19:44 -0400

    fixes recurrence type population script

--- foundation-database/public/patches/populate_recur.sql
+++ foundation-database/public/patches/populate_recur.sql
@@ -3,4 +3,4 @@ INSERT INTO recurtype (recurtype_type, recurtype_table, recurtype_donecheck,
                        recurtype_copyargs, recurtype_delfunc) 
                VALUES ('SALESORDER', 'cohead', 'isRecurSODone(cohead_id)', 'cohead_orderdate',
                        NULL, 'copycohead', '{INTEGER,TIMESTAMP WITH TIME ZONE}', NULL) 
-          ON CONFLICT (recurtype_id) DO NOTHING;
+          ON CONFLICT (recurtype_type) DO NOTHING;
