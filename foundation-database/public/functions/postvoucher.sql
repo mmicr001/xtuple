f421936dd8754dabb8d285889578434d1fc3c557
Michael Micros <mmicros@xtuple.com>
2018-11-08 09:31:41 -0500

    fix spacing

--- foundation-database/public/functions/postvoucher.sql
+++ foundation-database/public/functions/postvoucher.sql
@@ -67,7 +67,7 @@ BEGIN
   _glDate := COALESCE(_p.vohead_gldistdate, _p.vohead_distdate);
 
 --  Disallow posting voucher if it has any voucherItems without any tagged receipts or distributions
-  IF ( EXISTS ( SELECT 1 
+  IF (EXISTS ( SELECT 1 
                   FROM voitem 
                  WHERE voitem_vohead_id = pVoheadId 
                    AND (NOT EXISTS(SELECT 1 
@@ -77,7 +77,7 @@ BEGIN
                         OR NOT EXISTS(SELECT 1 
                                         FROM vodist 
                                        WHERE vodist_vohead_id = voitem_vohead_id 
-                                         AND vodist_poitem_id = voitem_poitem_id ))))THEN 
+                                         AND vodist_poitem_id = voitem_poitem_id )))) THEN 
     RAISE EXCEPTION 'Cannot post Voucher #% as it has items without any tagged receipts or without distributions [xtuple: postVoucher, -11, %]',
 			_p.vohead_number, _p.vohead_number;
   END IF;
