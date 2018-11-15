b0fba27dca073d47912e82dd2d869ca3612511ca
Michael Micros <mmicros@xtuple.com>
2018-11-15 17:05:47 -0500

    keep urlinfo and file sequences in sync

--- foundation-database/public/functions/createfile.sql
+++ foundation-database/public/functions/createfile.sql
@@ -3,8 +3,20 @@ drop function if exists createFile(TEXT, TEXT, BYTEA) cascade;
 create or replace function createFile(pTitle TEXT, pDescription TEXT, pStream BYTEA, pMimeType TEXT DEFAULT NULL) returns integer as $$
 declare
   _id integer;
+  pDefault text;
 begin
-  _id := nextval('file_file_id_seq');
+  select column_default 
+    into pDefault 
+      FROM INFORMATION_SCHEMA.COLUMNS 
+    where table_name = 'file' 
+      and column_name='file_id';
+
+  if pDefault like '%file_id%'
+    then _id := nextval('file_file_id_seq');
+  elsif(pDefault like '%url_id%') 
+    then _id := nextval('urlinfo_url_id_seq');
+  end if;   
+  
   insert into file (file_id, file_title, file_descrip, file_stream, file_mime_type) values (_id, pTitle, pDescription, pStream, COALESCE(pMimeType, 'application/octet-stream'));
   return _id;
 end;
--- foundation-database/public/functions/createurl.sql
+++ foundation-database/public/functions/createurl.sql
@@ -3,8 +3,20 @@ declare
   pTitle ALIAS FOR $1;
   pUrl ALIAS FOR $2;
   _id integer;
+  pDefault text;
 begin
-  _id := nextval('urlinfo_url_id_seq');
+  select column_default 
+    into pDefault 
+      FROM INFORMATION_SCHEMA.COLUMNS 
+    where table_name = 'urlinfo' 
+      and column_name='url_id';
+
+  if pDefault like '%file_id%'
+    then _id := nextval('file_file_id_seq');
+  elsif(pDefault like '%url_id%') 
+    then _id := nextval('urlinfo_url_id_seq');
+  end if;   
+  
   insert into urlinfo (url_id, url_title, url_url) values (_id, pTitle, pUrl);
   return _id;
 end;
