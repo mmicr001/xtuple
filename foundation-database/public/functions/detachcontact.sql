51832c47ca764450d9d271557e9aea01794bad0c
Dave Anderson <dave@pentuple.co.nz>
2018-11-22 10:24:50 +1300

    Detach Contact switched from delete assignment to mark inactive. Needed to update the comments posting at the same time.

--- foundation-database/public/functions/detachcontact.sql
+++ foundation-database/public/functions/detachcontact.sql
@@ -5,24 +5,10 @@ CREATE OR REPLACE FUNCTION detachContact(pAssignmentId INTEGER)
 RETURNS INTEGER AS $$
 -- Copyright (c) 1999-2018 by OpenMFG LLC, d/b/a xTuple. 
 -- See www.xtuple.com/CPAL for the full text of the software license.
-DECLARE
- _rec    RECORD;
 BEGIN
-
--- Cache full-text information
-  SELECT cntct_id, cntct_name, crmacct_name, crmrole_name INTO _rec
-    FROM crmacctcntctass
-    JOIN cntct   ON cntct_id   = crmacctcntctass_cntct_id
-    JOIN crmacct ON crmacct_id = crmacctcntctass_crmacct_id
-    JOIN crmrole ON crmrole_id = crmacctcntctass_crmrole_id
-   WHERE crmacctcntctass_id = pAssignmentId;
 
   UPDATE crmacctcntctass SET crmacctcntctass_active = FALSE
   WHERE crmacctcntctass_id = pAssignmentId;
-
-  PERFORM postComment('ChangeLog', 'T', _rec.cntct_id,
-                      format('Assignment Updated: %s was marked as inactive in Account %s',
-                             _rec.cntct_name, _rec.crmacct_name));
 
   RETURN 0;
 END;
