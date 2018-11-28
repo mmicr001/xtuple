8e6a119c51c8ffb3adae9d8fb75b651aa58f73f1
Michael Micros <mmicros@xtuple.com>
2018-11-28 09:38:00 -0500

    make use of default

--- foundation-database/public/functions/createfile.sql
+++ foundation-database/public/functions/createfile.sql
@@ -2,22 +2,8 @@ drop function if exists createFile(TEXT, TEXT, BYTEA) cascade;
 
 create or replace function createFile(pTitle TEXT, pDescription TEXT, pStream BYTEA, pMimeType TEXT DEFAULT NULL) returns integer as $$
 declare
-  _id integer;
-  pDefault text;
 begin
-  select column_default 
-    into pDefault 
-      FROM INFORMATION_SCHEMA.COLUMNS 
-    where table_name = 'file' 
-      and column_name='file_id';
-
-  if pDefault like '%file_id%'
-    then _id := nextval('file_file_id_seq');
-  elsif(pDefault like '%url_id%') 
-    then _id := nextval('urlinfo_url_id_seq');
-  end if;   
-  
-  insert into file (file_id, file_title, file_descrip, file_stream, file_mime_type) values (_id, pTitle, pDescription, pStream, COALESCE(pMimeType, 'application/octet-stream'));
-  return _id;
+  insert into file ( file_title, file_descrip, file_stream, file_mime_type) values ( pTitle, pDescription, pStream, COALESCE(pMimeType, 'application/octet-stream'));
+  return currval('file_file_id_seq');
 end;
 $$ language 'plpgsql';
--- foundation-database/public/functions/createurl.sql
+++ foundation-database/public/functions/createurl.sql
@@ -2,22 +2,8 @@ create or replace function createUrl(text, text) returns integer as $$
 declare
   pTitle ALIAS FOR $1;
   pUrl ALIAS FOR $2;
-  _id integer;
-  pDefault text;
 begin
-  select column_default 
-    into pDefault 
-      FROM INFORMATION_SCHEMA.COLUMNS 
-    where table_name = 'urlinfo' 
-      and column_name='url_id';
-
-  if pDefault like '%file_id%'
-    then _id := nextval('file_file_id_seq');
-  elsif(pDefault like '%url_id%') 
-    then _id := nextval('urlinfo_url_id_seq');
-  end if;   
-  
-  insert into urlinfo (url_id, url_title, url_url) values (_id, pTitle, pUrl);
-  return _id;
+  insert into urlinfo ( url_title, url_url) values ( pTitle, pUrl);
+  return currval('file_file_id_seq');
 end;
 $$ language 'plpgsql';
