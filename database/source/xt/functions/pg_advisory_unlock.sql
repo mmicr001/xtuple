6696ca187964113ad62e3a928a56329c95112fa8
Gil Moskowitz <gilmoskowitz@acm.org>
2019-02-19 10:41:32 -0500

    [34341] respond to code review comment

--- database/source/xt/functions/pg_advisory_unlock.sql
+++ database/source/xt/functions/pg_advisory_unlock.sql
@@ -4,13 +4,16 @@ CREATE OR REPLACE FUNCTION xt.pg_advisory_unlock(pOid INTEGER, pId INTEGER)
 -- Copyright (c) 1999-2019 by OpenMFG LLC, d/b/a xTuple. 
 -- See www.xtuple.com/CPAL for the full text of the software license.
 -- simplified version of lib/orm/source/xt/javascript/data.sql::releaseLock()
+DECLARE
+  _rows INTEGER;
+  _unlocked BOOLEAN := pg_catalog.pg_advisory_unlock(pOid, pId);
 BEGIN
-  PERFORM pg_catalog.pg_advisory_unlock(pOid, pId);
   DELETE FROM xt.lock
    WHERE lock_table_oid = pOid
      AND lock_record_id = pId
      AND lock_username  = geteffectivextuser()
      AND lock_pid       = pg_backend_pid();
-  RETURN TRUE;
+  GET DIAGNOSTICS _rows = ROW_COUNT;
+  RETURN _rows > 0 OR _unlocked;
 END;
 $$ LANGUAGE plpgsql;
