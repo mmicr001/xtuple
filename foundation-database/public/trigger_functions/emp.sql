bdebfd5d95171adc2192a5b341d47c34cb2c6ff4
Dave Anderson <dave@pentuple.co.nz>
2019-03-15 08:31:57 +1300

    Fix stoopid cherry-pick error

--- foundation-database/public/trigger_functions/emp.sql
+++ foundation-database/public/trigger_functions/emp.sql
@@ -174,7 +174,7 @@ BEGIN
 
   DELETE FROM docass WHERE docass_source_id = OLD.emp_id AND docass_source_type = 'EMP';
   DELETE FROM docass WHERE docass_target_id = OLD.emp_id AND docass_target_type = 'EMP';
-  DELETE FROM empgrpitem WHERE groupsitem_reference_id = OLD.emp_id;
+  DELETE FROM empgrpitem WHERE empgrpitem_emp_id = OLD.emp_id;
 
   RETURN OLD;
 END;
