1d7ea82514119996081c6855ac24851866cd504c
Dave Anderson <dave@pentuple.co.nz>
2019-03-26 08:11:18 +1300

    Amend function capitalisation to allow CI to pass

--- foundation-database/public/functions/validatemailaddress.sql
+++ foundation-database/public/functions/validatemailaddress.sql
@@ -1,4 +1,4 @@
-CREATE OR REPLACE FUNCTION validateEmailAddress(pEmail TEXT) 
+CREATE OR REPLACE FUNCTION validateemailaddress(pEmail TEXT)
 RETURNS BOOLEAN AS $$
 -- Copyright (c) 1999-2019 by OpenMFG LLC, d/b/a xTuple. 
 -- See www.xtuple.com/CPAL for the full text of the software license.
