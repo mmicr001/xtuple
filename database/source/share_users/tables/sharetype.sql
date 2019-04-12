2866ccc5a2dadab306a4058792c302210086151c
Ben Thompson <ben@xtuple.com>
2019-04-12 14:12:30 -0400

    [33398] Move mobile client crm extension ORM code into core 'database' directory.

--- database/source/share_users/tables/sharetype.sql
+++ database/source/share_users/tables/sharetype.sql
@@ -11,3 +11,29 @@ insert into xt.sharetype (
   'obj_uuid',
   'username'
 );
+
+-- Contact CRM Account's users.
+insert into xt.sharetype (
+  sharetype_nsname,
+  sharetype_tblname,
+  sharetype_col_obj_uuid,
+  sharetype_col_username
+) values (
+  'xt',
+  'share_users_cntct',
+  'obj_uuid',
+  'username'
+);
+
+-- Address CRM Account's users.
+insert into xt.sharetype (
+  sharetype_nsname,
+  sharetype_tblname,
+  sharetype_col_obj_uuid,
+  sharetype_col_username
+) values (
+  'xt',
+  'share_users_addr',
+  'obj_uuid',
+  'username'
+);
